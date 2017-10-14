79a0d411e57e3a6ef7dc529de6c0e7acbcbeb504
Jean Georges Perrin <jgp@jgp.net>
2017-10-14 07:03:56 -0400

    Initial code.

---
+++ labs-spark-datasources/src/main/java/net/jgp/labs/spark/datasources/l100_photo_datasource/PhotoMetadataIngestionApp.java
@@ -0,0 +1,13 @@
+package net.jgp.labs.spark.datasources.l100_photo_datasource;
+
+public class PhotoMetadataIngestionApp {
+    public static void main(String[] args) {
+        PhotoMetadataIngestionApp app = new PhotoMetadataIngestionApp();
+        app.start();
+    }
+
+    private boolean start() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
